90b571149608b00576f5dba357283489f4d0a83e
courtmountain <benedikt-hofberger@web.de>
2022-08-16 12:51:17 +0200

    Init

---
+++ MvcRazorComponent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcRazorComponent.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
